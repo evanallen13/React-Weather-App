90e01a29974b8c1fdf8ecade4778f3d124b59cc5
Evan Allen <evanallen13@gmail.com>
2019-08-10 12:30:31 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Weather from './components/weather'
+import Form from './components/form'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'weather-icons/css/weather-icons.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const APIkey = 'b302a0a87aabb08b4029ea03af6e4705';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      city: undefined,
+      country: undefined,
+      icon: undefined,
+      main: undefined,
+      celsius: undefined,
+      temp_max: null,
+      temp_min: null,
+      description: "",
+      error: false,
+      tempForm : 'F'
+    }
+    this.weatherIcon = {
+      Thunderstorm: "wi-thunderstorm",
+      Drizzle: "wi-sleet",
+      Rain: "wi-storm-showers",
+      Snow: "wi-snow",
+      Atmosphere: "wi-fog",
+      Clear: "wi-day-sunny",
+      Clouds: "wi-day-fog"
+    }
+  }
+  tempFormChange = (deg) =>{
+    this.setState({
+      tempForm : deg
+    })
+  }
+
+  getWeatherIcon(icon,rangeId){
+    switch(true){
+      case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: this.weatherIcon.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: this.weatherIcon.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: this.weatherIcon.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: this.weatherIcon.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: this.weatherIcon.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: this.weatherIcon.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: this.weatherIcon.Clouds });
+        break;
+      default:
+        this.setState({ icon: this.weatherIcon.Clouds });
+    }
+
+  }
+
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+
+    if(city && country){
+      const api_call = await fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${APIkey}`
+        )
+      const response = await api_call.json()
+      console.log(response)
+  
+      this.setState({
+        city : response.name,
+        country : response.sys.country,
+        main: undefined,
+        celsius: response.main.temp,
+        temp_max: response.main.temp_max,
+        temp_min: response.main.temp_min,
+        description: response.weather[0].description,
+        icon: this.weatherIcon.Thunderstorm,
+        weatherId : response.weather[0].id,
+      })
+      this.getWeatherIcon(this.weatherIcon,response.weather[0].id)
+    }else{
+      this.setState({
+        error : true
+      })
+    }
+  }
+  render() { 
+    return (
+      <div className="App">
+        <Form
+          loadWeather={this.getWeather}
+          error={this.state.error}
+        ></Form>
+        <Weather
+          cityName={this.state.city}
+          countryName={this.state.country}
+          temp_celius={this.state.celsius}
+          temp_min = {this.state.temp_min}
+          temp_max = {this.state.temp_max}
+          description = {this.state.description}
+          weatherIcon = {this.state.icon}
+          weatherId = {this.state.weatherId}
+          tempForm = {this.state.tempForm}
+        ></Weather>
+        {this.state.city ? (
+            <div className='tempSelect'>
+            <p id='F'
+              onClick={() =>{
+                this.tempFormChange('F')
+              }}
+            >F&deg;</p>
+            <p id="C"
+              onClick={() =>{
+                let deg = 'C'
+                this.tempFormChange(deg)
+              }}
+            >C&deg;</p>
+            
+          </div>
+        ):null}
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/components/weather.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+const Weather = (props) => {
+    return (
+        <div className="container">
+            <div className="cards">
+                {/* City Name */}
+                {props.cityName ? (
+                    <h1>{props.cityName},{props.countryName}</h1>
+                ):null}
+                {/* Weather Icon */}
+                <h5 className="py-4">
+                    <i className={`wi ${props.weatherIcon} display-1`}></i>
+                </h5>
+
+                {/* Degree */}
+                  {props.temp_celius ? (<h1 className="py-2">{tempCalc(props.temp_celius,props.tempForm)}&deg; {props.tempForm}</h1>):null}
+                  
+                {/* Max and Min Temp */}
+                {props.temp_celius ? (minMaxTemp(tempCalc(props.temp_min,props.tempForm),tempCalc(props.temp_max,props.tempForm))):null}
+                {/* Weather Description */}
+                <h4 className="py-3">{props.description}</h4>
+                {
+                    changeBackground(props.weatherId)
+                }
+            </div>
+        </div>
+    )
+}
+
+function minMaxTemp(min,max){
+    return(
+        <h3>
+            <span className="px-4">{min}&deg;</span>
+            <span className="px-4">{max}&deg;</span>
+        </h3>
+    )
+}
+function changeBackground(rangeId){
+    console.log(rangeId)
+    switch(true){
+        case rangeId >= 200 && rangeId < 232:
+            document.body.style.backgroundImage = "url(../imgs/thunder.gif)";
+          break;
+        case rangeId >= 300 && rangeId <= 321:
+            document.body.style.backgroundImage = "url(../imgs/rain.gif)";
+          break;
+        case rangeId >= 500 && rangeId <= 521:
+            document.body.style.backgroundImage = "url(../imgs/rain.gif)";
+          break;
+        case rangeId >= 600 && rangeId <= 622:
+            document.body.style.backgroundImage = "url(../imgs/snow.gif)";
+          break;
+        case rangeId >= 701 && rangeId <= 781:
+            document.body.style.backgroundImage = "url(../imgs/fog.jpg)";
+          break;
+        case rangeId === 800:
+            document.body.style.backgroundImage = "url(../imgs/sun.jpg)";
+          break;
+        case rangeId >= 801 && rangeId <= 804:
+            document.body.style.backgroundImage = "url(../imgs/cloudy.jpg";
+          break;
+        default:
+            document.body.style.backgroundColor = "#20B2AA";
+      }
+}
+function tempCalc (temp,form) {
+    let calcTemp = ''
+    if(form === 'F'){
+      calcTemp = Math.floor(((temp - 273.15) * 9/5) + 32)
+    }else{
+      calcTemp = Math.floor(temp - 273.15)
+    }
+    return calcTemp;
+  }
+
+export default Weather;
